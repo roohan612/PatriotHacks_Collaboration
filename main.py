dccff092965f573f4b5c3f541b320e4fee785e66
BrianSJr <36824030+BrianSJr@users.noreply.github.com>
2023-10-07 12:56:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-import pygame
+import pygame, sys
 
-if __name__ == "__main__":
+# initializes pygame
+pygame.init() 
 
-  print("Hello World")
-  print('Hi my name is Sai')
+
+# window creation, attributes, and setup:
+window : pygame.Surface             = pygame.display.set_mode()
+WINDOW_SIZE : tuple[int, int]       = window.get_size()
+ASPECT_RATIO : float                = window.get_width() / window.get_height()
+WINDOW_TITLE : str                  = "Exploit"
+FRAME_RATE : int                    = 60
+
+pygame.display.set_caption(WINDOW_TITLE)
+
+
+# clock
+clock : pygame.time.Clock = pygame.time.Clock()
+
+
+#game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    window.fill((0, 0, 0)) 
+    
+    pygame.display.update()
+    clock.tick(60)
+    
